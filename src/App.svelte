e1a415febf8d5fe93ba2deb5b38ba3d3be1e7c1c
jefo <jefo.work@gmail.com>
2022-07-23 13:02:46 +0300

    :rocket: fonts

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,40 @@
 <script>
+	import {onMount} from 'svelte';
+	import Accordion from 'accordion-js';
+	import 'accordion-js/dist/accordion.min.css';
 	import Layer from "./components/Layer.svelte";
+
+	onMount(() => {
+		new Accordion('.layers-list');
+	})
 </script>
 
 <main>
 	<div class="flex">
-		<div class="w-3/4 p-2">
-			<div class="content-center">Drop Images Here</div>
-		</div>
 		<div class="w-1/4">
 			<div class="text-lg">Layers</div>
-			<div class="pr-4">
+			<div class="pr-4 layers-list">
 				<div class="mb-2">
-					<Layer title="Backgrounds" />
+					<Layer id="1" title="Backgrounds" />
 				</div>
 				<div class="mb-2">
-					<Layer title="Backgrounds" />
+					<Layer id="2" title="Body" />
 				</div>
 				<div class="mb-2">
-					<Layer title="Backgrounds" />
+					<Layer title="Clothes" />
 				</div>
 				<div class="mb-2">
-					<Layer title="Backgrounds" />
-				</div>
+					<Layer id="3" title="Eyes" />
+				</div> 
 			</div>
+		</div>
+		<div class="w-3/4 p-2">
+			<div class="content-center">Drop Images Here</div>
 		</div>
 	</div>
 </main>
 <style windi:preflights:global windi:safelist:global>
+	main {
+		font-family: Cera Round Pro Regular;
+	}
 </style>
--- src/components/Layer.svelte
+++ src/components/Layer.svelte
@@ -2,11 +2,39 @@
     export let title = 'No title';
     export let count = 0;
     export let rarity = 100;
+    export let id = 0;
+    export let items = [
+        { title: "Body Type 1", count: 10, rarity: 100 },
+        { title: "Body Type 2", count: 12, rarity: 100 },
+        { title: "Body Type 3", count: 15, rarity: 100 },
+    ];
 </script>
 
-<button class="block flex justify-between w-full px-4 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80">
-    <span>{title}</span>
-    <span class="flex-1 flex-shrink-0"></span>
-    <span class="text-xs border rounded p-1 mr-1">{count}</span>
-    <span class="text-xs border rounded p-1">{rarity}</span>
-</button>
+<div>
+    <button class="layer block flex justify-between w-full px-4 py-2 font-medium tracking-wide text-black capitalize transition-colors duration-200 transform -md focus:outline-none focus:ring-opacity-80">
+        <span>{title}</span>
+    </button>
+    <div class="dropdown">
+        {#each items as item, index}
+        <div class="flex p-2">
+            <input type="checkbox" id="cb-{id}-{index}" />
+            <label for="cb-{id}-{index}">{item.title}</label>
+            <span class="flex-1 flex-shrink-0"></span>
+            <span class="badge text-xs border rounded p-1 mr-1">{item.count}</span>
+            <span class="badge text-xs border rounded p-1">{item.rarity}</span>
+        </div>
+        {/each}
+    </div>
+</div>
+
+<style>
+    .layer {
+        background-color: rgb(255, 212, 59);
+        border: 2px solid rgb(24, 49, 83);
+        border-radius: 12px;
+    }
+
+    .badge {
+        border: 1px solid rgb(24, 49, 83);
+    }
+</style>
